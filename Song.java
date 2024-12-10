ed40718341e004bd819398023c76a109e8d8975c
ishh97 <ishita.161204@gmail.com>
2024-12-10 11:51:30 +0530

    Static Varibles

--- Song.java
+++ Song.java
@@ -2,11 +2,15 @@ public class Song {
     private String title;
     private String artist;
     private String genre;
+    
+    private static int songCounter = 0;
 
     public Song(String title, String artist, String genre) {
         this.title = title;
         this.artist = artist;
         this.genre = genre;
+
+        songCounter++;
     }
 
     public void setSongDetails(String title, String artist, String genre) {
@@ -17,11 +21,14 @@ public class Song {
 
     public String displaySongDetails() {
         System.out.println("Title: " + title + ", Artist: " + artist + ", Genre: " + genre);
-
         return "";
     }
 
     public String getTitle() {
         return title;
+    }
+
+    public static int getSongCounter() {
+        return songCounter;
     }
 }
