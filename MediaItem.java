0230ca9fca056a0213bd2889877ea893956fa671
ishh97 <ishita.161204@gmail.com>
2024-12-11 09:35:47 +0530

    Abstraction

---
+++ MediaItem.java
@@ -0,0 +1,27 @@
+public abstract class MediaItem {
+    private String title;
+    private String genre;
+
+    public MediaItem(String title, String genre) {
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public abstract void displayDetails();
+}
--- Song.java
+++ Song.java
@@ -1,24 +1,13 @@
-public class Song {
-    private String title;
+public class Song extends MediaItem {
     private String artist;
-    private String genre;
 
-    private static int songCounter = 0;
+    private static int songCounter = 0; 
 
     public Song(String title, String artist, String genre) {
-        this.title = title;
+        super(title, genre); 
         this.artist = artist;
-        this.genre = genre;
 
         songCounter++;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getArtist() {
@@ -29,16 +18,9 @@ public class Song {
         this.artist = artist;
     }
 
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public void displaySongDetails() {
-        System.out.println("Title: " + title + ", Artist: " + artist + ", Genre: " + genre);
+    @Override
+    public void displayDetails() {
+        System.out.println("Title: " + getTitle() + ", Artist: " + artist + ", Genre: " + getGenre());
     }
 
     public static void displayTotalSongs() {
