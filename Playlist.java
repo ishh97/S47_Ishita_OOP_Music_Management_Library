7802a884e749879bb8a86171086c06e28ca20bdb
ishh97 <ishita.161204@gmail.com>
2024-12-11 09:45:06 +0530

    Constructors and Destructors

--- Playlist.java
+++ Playlist.java
@@ -6,18 +6,30 @@ public class Playlist {
 
     private static int playlistCount = 0;
 
+    public Playlist() {
+        this.name = "Untitled Playlist";
+        this.songs = new ArrayList<>();
+        playlistCount++;
+        System.out.println("Default Playlist Constructor Called");
+    }
+
     public Playlist(String name) {
         this.name = name;
         this.songs = new ArrayList<>();
         playlistCount++;
+        System.out.println("Parameterized Playlist Constructor Called");
     }
 
     public String getName() {
         return name;
     }
 
-    public void setPlaylistName(String name) {
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
     }
 
     public void addSong(Song song) {
@@ -33,5 +45,10 @@ public class Playlist {
 
     public static int getPlaylistCount() {
         return playlistCount;
+    }
+
+    public void cleanup() {
+        System.out.println("Cleaning up resources for Playlist: " + name);
+        songs.clear();
     }
 }
